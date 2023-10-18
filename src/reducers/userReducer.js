7c942ba5433989bf4800664d752f14330d37c9ab
Tudor181 <tudor.alin45@gmail.com>
2023-10-18 23:46:59 +0300

    new action

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,18 +1,21 @@
-import { USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from "../Constants/userConstants";
+import {
+  USER_LOGOUT_SUCCESS,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+} from "../Constants/userConstants";
 
-
-export const userReducer = (state={},action) =>{
-
-    switch(action.type)
-    {
-        case USER_REGISTER_REQUEST:
-            return {loading:true,error:false}
-        case USER_REGISTER_SUCCESS:
-            return {loading:false,error:false,user:action.payload}
-        case USER_REGISTER_FAIL:
-            return {loading:false,error:action.payload}
-        default:
-            return {}
-    }
-
-}
+export const userReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true, error: false };
+    case USER_REGISTER_SUCCESS:
+      return { loading: false, error: false, user: action.payload };
+    case USER_REGISTER_FAIL:
+      return { loading: false, error: action.payload };
+    case USER_LOGOUT_SUCCESS:
+      return { loading: false, error: false };
+    default:
+      return {};
+  }
+};
